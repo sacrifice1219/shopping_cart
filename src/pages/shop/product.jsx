c08aeae9cfc67603fccaecc6128910d0f2c38f9c
lucky favour <luckyfavour1219@gmail.com>
2023-11-05 19:19:32 -0800

    commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./shop.css";
+
+const Product = (props) => {
+    const {id, productName, price, productImage} = props.data;
+  return (
+    <div className='product'>
+        <img src={productImage} />
+        <div className="description">
+            <p>
+                <b>
+                    {productName}
+                </b>
+                </p>
+                <p>${price}</p>
+        </div>
+        <button className="addToCartBttn">Add To Cart</button>
+    </div>
+  )
+}
+
+export default Product;
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { PRODUCTS } from '../../products';
+import './shop.css';
+import Product from './product';
+
+const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className="shopTitle">
+            <h1>Tech Shop</h1>
+        </div>
+
+        <div className="products">
+            {PRODUCTS.map((product) => ( <Product data={product} /> ))}
+        </div>
+    </div>
+  )
+}
+
+export default Shop;
